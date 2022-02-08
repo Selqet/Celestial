a72432de85e6ece56371ac2434e08e6e1641d65d
Selqet <75011650+Selqet@users.noreply.github.com>
2022-02-08 20:54:26 +0300

    Initial commit

---
+++ lib/network/derpibooru_parser.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class PicturesURL {
+  List<String>? _picturesURL;
+
+}
---
+++ lib/ui/screens/main_gallery.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MainGallery extends StatefulWidget {
+  const MainGallery({Key? key}) : super(key: key);
+
+  @override
+  _MainGalleryState createState() => _MainGalleryState();
+}
+
+class _MainGalleryState extends State<MainGallery> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.search),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(hintText: 'Please enter some text'),
+              ),
+            )
+          ],
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
